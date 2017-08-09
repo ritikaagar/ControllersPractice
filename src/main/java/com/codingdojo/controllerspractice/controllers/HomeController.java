537ce08c1fbaf113bffd24a949603f1ca3deeb9f
ritikaagar <Ritika@Ritikas-MacBook-Pro.local>
2017-08-08 17:33:19 -0700

    Controllers practice commit

---
+++ src/main/java/com/codingdojo/controllerspractice/controllers/HomeController.java
@@ -0,0 +1,14 @@
+package com.codingdojo.controllerspractice.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class HomeController {
+	@RequestMapping("/")
+	public String index() {
+		return "index.html";
+	
+	}
+}
